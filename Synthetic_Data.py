4d3a18d84a66f24c39ab5c8ca5fdc451eba03d43
emkloco <mkjrftw@gmail.com>
2025-10-13 21:24:58 +0100

    even lower noise

--- Synthetic_Data.py
+++ Synthetic_Data.py
@@ -10,7 +10,7 @@ x_range = (0,10)
 
 X = np.linspace(x_range[0], x_range[1], num_points)
 
-noise = np.random.normal(0,0.3, num_points)
+noise = np.random.normal(0,0.1, num_points)
 Y = m * X + b + noise
 df = pd.DataFrame ({'X': X, 'Y':Y})
 df.to_csv('synthetic_data.csv', index=False)
