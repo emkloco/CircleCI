1a702162565ec176430a03ed0e8bae09914483fc
emkloco <mkjrftw@gmail.com>
2025-10-13 21:15:43 +0100

    change of m and b and noise to see new unit test result on ci pipeline

--- Synthetic_Data.py
+++ Synthetic_Data.py
@@ -2,15 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-m = 1.5
-b = 2.0
+m = 3.5
+b = 4.0
 
 num_points = 67
 x_range = (0,10)
 
 X = np.linspace(x_range[0], x_range[1], num_points)
 
-noise = np.random.normal(0,1, num_points)
+noise = np.random.normal(0,3, num_points)
 Y = m * X + b + noise
 df = pd.DataFrame ({'X': X, 'Y':Y})
 df.to_csv('synthetic_data.csv', index=False)
